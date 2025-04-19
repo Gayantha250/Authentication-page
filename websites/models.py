887199126df57458c8f32008007d8345ae1777b2
Gayantha250 <gayantha355@gmail.com>
2025-04-19 13:00:46 +0200

    setup dtabase table

--- websites/models.py
+++ websites/models.py
@@ -1,6 +1,20 @@
+import datetime
+
 from . import db
 from flask_login import UserMixin
 
+
+class Note(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    data=db.Column(db.String(1000))
+    date=db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    email= db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    notes= db.relationship('Note')
+
